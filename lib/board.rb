e6a3ce0124b13cff716979e3903d53bc3c95d4b1
Guilherme Sa <guilsa001@gmail.com>
2015-09-28 11:49:08 -0400

    Add board #state

---
+++ lib/board.rb
@@ -0,0 +1,7 @@
+class Board
+  attr_accessor :state
+  def initialize
+    @state = Array.new(9, " ")
+  end
+
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+describe "state" do
+  it "should return a board state" do
+    board = Board.new
+    expect(board.state).to eq([" ", " ", " "," ", " ", " "," ", " ", " "])
+  end
+end
+
+# describe "make_board_state" do
+#   let(:board) { Board.new }
+#   xit "should return an array representing new board state given a play position" do
+#     board.make_board_state(5)
+#   end
+# end
